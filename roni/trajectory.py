bea5f40afeff42556d59123a63a258003848ab2c
Yoni Zohar <yonizohar@Yonis-MacBook-Pro.local>
2022-10-24 11:46:21 +0300

    no errors

--- roni/trajectory.py
+++ roni/trajectory.py
@@ -19,11 +19,9 @@ precon_to_states = {}
 # preconditions
 preconditions = {}
 for i in range(len(effects)):
-  preconditions[i] = []
-  for j in range(num_of_state_variables):
-    new_precon_var = Bool("f" + str(j) + "_in_precon_of_action_" + str(i))
-    preconditions[i] += [new_precon_var]
-    precon_to_states[new_precon_var] = j
+  for s in range(num_of_state_variables):
+    new_precon_var = Bool("f" + str(s) + "_in_precon_of_action_" + str(i))
+    preconditions[(i,s)] = new_precon_var
 
 
 print(precon_to_states)
@@ -35,7 +33,7 @@ trajectories = [
 [[True, True], 0, [True, False], 0, [True, False]],
 # ...
 # last trajectory
-[False, True], 0, [False, False], 0, [False, False]
+[[False, True], 0, [False, False], 0, [False, False]]
 ]
 
 
@@ -43,24 +41,18 @@ solver = Solver()
 
 
 def consistent(preconditions, trajectories):
+  axiom1_instances = []
   state_variable_in_trajectory_step = {}
   for i in range(num_of_state_variables):
     for j, trajectory in enumerate(trajectories):
+      print("panda j: ", str(j))
+      print("panda trajectory: ", str(trajectory))
       for k in range(int((len(trajectory) - 1) / 2)):
         state_variable_in_trajectory_step[(i,j,k)] = Bool("state_var_" + str(i) + "_in_traj_" + str(j) + "_on_step_" + str(k))
-  print(state_variable_in_trajectory_step)
-
-  is_state = True
-  axiom1_instances = []
-  for i, elem in enumerate(trajectory):
-    if not is_state:
-      # is_state is False, we are in action
-      # axiom 1:
-      assert(i > 0)
-      pre_state = trajectory[i-1]
-      for lit in preconditions[elem]:
-         axiom1 = Implies(lit, precon_to_states[lit])
-         axiom1_instances += [axiom1]
+        action = trajectory[2*k+1]
+        precondition_var = preconditions[action, i]
+        axiom1 = Implies(state_variable_in_trajectory_step[(i,j,k)], preconditions[action,i])
+        axiom1_instances += [axiom1]
       
       # # axiom 2:
       # assert(i < len(trajectory) - 1)
@@ -70,7 +62,6 @@ def consistent(preconditions, trajectories):
 
       # # axiom 3:
       # # TODO
-    is_state = not is_state
   return And(axiom1_instances)
 
 # the action model given by the preconditions is safe for plans that have n steps
@@ -83,26 +74,23 @@ def consistent(preconditions, trajectories):
 solver = Solver()
 cons = []
 result = consistent(preconditions, trajectories)
-cons += [result]
-cons = And(cons)
-solver.add(cons)
-print("constraints:", cons)
+solver.add(result)
+print("constraints:", result)
 result = solver.check()
 while result == sat:
   print(result)
   model = solver.model()
-#   print("**********************\n", model)
-#   print("action model:", model)
-#   print("trajectories:", trajectories)
-#   print("effects:", effects)
+  print("**********************\n", model)
+  print("action model:", model)
+  print("trajectories:", trajectories)
+  print("effects:", effects)
   model_values = []
-  for i in range(0, len(preconditions)):
-    for j in range(0, len(preconditions[i])):
-      model_val = model[preconditions[i][j]]
+  for p in preconditions.values():
+      model_val = model[p]
       if model_val == True:
-        model_values += [preconditions[i][j]]
+        model_values += [p]
       else:
-        model_values += [Not(preconditions[i][j])]
+        model_values += [Not(p)]
   block_model = Not(And(model_values))
   solver.add(block_model)
   result = solver.check()
